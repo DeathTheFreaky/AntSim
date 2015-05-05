b53fcee1c5554897d9d6d573d3f45cc6d60e703d
Clemens <Ich>
2015-05-05 18:36:51 +0200

    changed InputWaitTime to match FPS

--- AntSim/src/at/antSim/Globals.java
+++ AntSim/src/at/antSim/Globals.java
@@ -1,8 +1,6 @@
 package at.antSim;
 
 import java.awt.*;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
 
 /**Stores global variables, mainly config file entries and constants like resource folder, fps_cap...
  * 
@@ -22,7 +20,7 @@ public final class Globals {
 	/**
 	 * duration of 1/60 second in nanoseconds
 	 */
-	public static final int FPS_60_DURATION_NANONS = (int) ((1.0 / 60) * 1000 * 1000 * 1000);
+	public static final int FPS_60_DURATION_NANONS = (int) ((1.0 / FPS_CAP) * 1000 * 1000 * 1000);
 	public static final float WORLD_SIZE = 800; //square
 	
 	//config file entries set by config reader
