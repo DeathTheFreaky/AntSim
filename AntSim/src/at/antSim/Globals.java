64e959d257c9c5feb1d5ecf333a094d804612d19
Clemens <Ich>
2015-05-05 18:38:40 +0200

    changed name of static FPS_Duration

--- AntSim/src/at/antSim/Globals.java
+++ AntSim/src/at/antSim/Globals.java
@@ -18,9 +18,9 @@ public final class Globals {
 	public static final String SHADERS = WORKING_DIR + "\\src\\at\\antSim\\graphics\\shaders\\";
 	public static final int FPS_CAP = 120;
 	/**
-	 * duration of 1/60 second in nanoseconds
+	 * duration of 1/FPS second in nanoseconds
 	 */
-	public static final int FPS_60_DURATION_NANONS = (int) ((1.0 / FPS_CAP) * 1000 * 1000 * 1000);
+	public static final int FPS_DURATION_NANONS = (int) ((1.0 / FPS_CAP) * 1000 * 1000 * 1000);
 	public static final float WORLD_SIZE = 800; //square
 	
 	//config file entries set by config reader
