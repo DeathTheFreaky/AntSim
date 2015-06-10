b8ec121fe1eecbd8a1f8f354ab7babf82b386521
flohil <florian.hilbinger@hotmail.com>
2015-06-10 13:17:52 +0200

    deleted flag output in GhostObject

--- AntSim/src/at/antSim/objectsPhysic/GhostPhysicsObject.java
+++ AntSim/src/at/antSim/objectsPhysic/GhostPhysicsObject.java
@@ -20,9 +20,7 @@ public class GhostPhysicsObject extends PositionablePhysicsObjectImpl {
 	public GhostPhysicsObject(GhostObject body, String type) {
 		this.body = body;
 		this.type = type;
-//		this.body.setCollisionFlags(CollisionFlags.NO_CONTACT_RESPONSE);
 		this.body.setCollisionFlags(this.body.getCollisionFlags() & CollisionFlags.NO_CONTACT_RESPONSE);
-		System.out.println("Flags creating: " + this.body.getCollisionFlags());
 		this.body.setUserPointer(this);
 	}
 
