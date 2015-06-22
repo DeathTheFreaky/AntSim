6c98f2a6f196fdbf07a24156433444022cfc6d73
flohil <florian.hilbinger@hotmail.com>
2015-06-22 16:43:57 +0200

    now supporting ordered transparents by z-value, but does not fully work

--- AntSim/src/at/antSim/objectsKI/Entity.java
+++ AntSim/src/at/antSim/objectsKI/Entity.java
@@ -26,6 +26,7 @@ public abstract class Entity {
 
 	static final Map<PhysicsObject, ObjectType> physicsObjectTypeMap = new HashMap<PhysicsObject, ObjectType>();
 	static final Map<TexturedModel, List<Entity>> renderingMap = new HashMap<TexturedModel, List<Entity>>();
+	static final LinkedList<Entity> sortedTransparents = new LinkedList<>();
 	static final Map<PhysicsObject, Entity> parentingEntities = new HashMap<PhysicsObject, Entity>(); //allows us to get eg. the Food Entity in a react() method when an ant hit the Food Entitie's physicsObject
 	static final List<Entity> entities = new LinkedList<>(); //used to delete all entities
 	static final List<Entity> dynamicEntities = new LinkedList<>();
@@ -47,7 +48,29 @@ public abstract class Entity {
 		physicsObjectTypeMap.put(physicsObject, type);
 		if (graphicsEntity != null) {
 			addRenderingEntity();
+			if (graphicsEntity.getModel().usesTransparency()) {
+				addTransparent(this);
+			}
 		}
+	}
+
+	/**Adds entity sorted by z-value in world position.
+	 * @param entity
+	 */
+	private void addTransparent(Entity entity) {
+		
+		int idx = 0;
+		ReadOnlyPhysicsObject po = (ReadOnlyPhysicsObject) entity.physicsObject;
+		
+		for (Entity sortedEntity : sortedTransparents){
+			ReadOnlyPhysicsObject sortedPo = (ReadOnlyPhysicsObject) sortedEntity.physicsObject;
+			if ((po.getPosition().z - entity.getGraphicsEntity().getScale()/2) > (sortedPo.getPosition().z - sortedEntity.getGraphicsEntity().getScale()/2)) {
+				break;
+			}
+			idx++;
+		}
+		
+		sortedTransparents.add(idx, entity);
 	}
 
 	public abstract void react(StaticPhysicsObject staticPhysicsObject);
@@ -89,6 +112,7 @@ public abstract class Entity {
 		physicsObjectTypeMap.remove(this);
 		if (graphicsEntity != null) { //null for Pheromones
 			renderingMap.get(graphicsEntity.getModel()).remove(this);
+			sortedTransparents.remove(this);
 		}
 	}
 	
@@ -102,6 +126,13 @@ public abstract class Entity {
 	 */
 	public static Map<TexturedModel, List<Entity>> getUnmodifiableRenderingMap() {
 		return Collections.unmodifiableMap(renderingMap);
+	}
+	
+	/**
+	 * @return - an unmodifiable version of renderingMap for preventing changes on renderingMap while allowing it to be retrieved for rendering
+	 */
+	public static LinkedList<Entity> getTransparentsList() {
+		return sortedTransparents;
 	}
 	
 	public static Entity getParentEntity(PhysicsObject physicsObject) {
@@ -129,6 +160,7 @@ public abstract class Entity {
 		}
 		physicsObjectTypeMap.clear();
 		renderingMap.clear();
+		sortedTransparents.clear();
 		parentingEntities.clear();
 		entities.clear();
 		dynamicEntities.clear();
